b71620166b5d4924252232253eb78f7acb5b0dfc
taufiqperlyard <32461641+taufiqperlyard@users.noreply.github.com>
2017-10-04 19:35:44 +0700

    editaccountinfo

---
+++ Projectbengkel/app/src/main/java/com/projectbengkel/projectbengkel/editaccountinfo.java
@@ -0,0 +1,13 @@
+package com.projectbengkel.projectbengkel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class editaccountinfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editaccountinfo);
+    }
+}
