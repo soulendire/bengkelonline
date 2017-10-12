ad06a2d99f996e8a90fdb59bbb1a1a806e846bfc
soulendire <prawira97@gmail.com>
2017-10-12 14:19:38 +0700

    qwfdhjgh

--- Projectbengkel/app/src/main/java/com/projectbengkel/projectbengkel/pickuploc.java
+++ Projectbengkel/app/src/main/java/com/projectbengkel/projectbengkel/pickuploc.java
@@ -3,11 +3,11 @@ package com.projectbengkel.projectbengkel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class callmechanic extends AppCompatActivity {
+public class pickuploc extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_callmechanic);
+        setContentView(R.layout.activity_pickuploc);
     }
 }
